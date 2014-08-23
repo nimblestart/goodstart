d60f0dffc031379974c0abf9e0ef34bd7aca4473
nimblestart <nimble.start@gmail.com>
2014-08-23 16:13:04 +0000

    test commit removed ls

--- features/lib/output_capture_helper.rb
+++ features/lib/output_capture_helper.rb
@@ -2,7 +2,7 @@
 #
 # @param [String] scenario value of the scenario object
 def capture_screenshot(scenario)
-  date_timelsls = Time.now.strftime('%Y%m%d_%H.%M.%S_')
+  date_time = Time.now.strftime('%Y%m%d_%H.%M.%S_')
   Dir::mkdir('screenshots') if not File.directory?('screenshots')
   screenshot_filename = "./screenshots/FAILED_#{date_time}_#{scenario.name.gsub(' ','_').gsub(/[^0-9A-Za-z_]/, '')}.png"
   @browser.save_screenshot screenshot_filename
